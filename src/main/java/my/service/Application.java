cf9a5a505c72fffdbe1e3e7d99d63c45dd7268a4
k2works <kakimomokuri@gmail.com>
2019-03-12 13:54:33 +0900

    feat: HeloControllerを作成する

--- src/main/java/my/service/Application.java
+++ src/main/java/my/service/Application.java
@@ -1,5 +1,6 @@
 package my.service;
 
+import my.service.controller.HeloController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
