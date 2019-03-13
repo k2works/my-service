a54f47b8973f8904d67a5f2f0649d2f25bc90853
k2works <kakimomokuri@gmail.com>
2019-03-13 17:38:51 +0900

    feat: Criteria APIによる検索
    orderByによるエンティティのソート

--- src/main/java/my/service/model/MyDataDaoImpl.java
+++ src/main/java/my/service/model/MyDataDaoImpl.java
@@ -29,7 +29,7 @@ public class MyDataDaoImpl implements MyDataDao<MyData> {
        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
        CriteriaQuery<MyData> query = builder.createQuery(MyData.class);
        Root<MyData> root = query.from(MyData.class);
-       query.select(root);
+       query.select(root).orderBy(builder.asc(root.get("name")));
        list = (List<MyData>)entityManager
                .createQuery(query)
                .getResultList();
