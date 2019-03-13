dc35dfb36c89621110191c6b6994711d560736eb
k2works <kakimomokuri@gmail.com>
2019-03-13 12:28:52 +0900

    feat: JPAによるデータベースの利用
    リポジトリクラスMyDataRepositoryを作成する

---
+++ src/main/java/my/service/repository/MyDataRepository.java
@@ -0,0 +1,9 @@
+package my.service.repository;
+
+import my.service.model.MyData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MyDataRepository extends JpaRepository<MyData, Long> {
+}
