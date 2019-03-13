1dc3f1b56eda14fa0beb2168fcecd248a298ec39
k2works <kakimomokuri@gmail.com>
2019-03-13 14:28:08 +0900

    feat: エンティティのCRUD
    JpaRepositoryのメソッド実装例

--- src/main/java/my/service/repository/MyDataRepository.java
+++ src/main/java/my/service/repository/MyDataRepository.java
@@ -4,11 +4,16 @@ import my.service.model.MyData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MyDataRepository extends JpaRepository<MyData, Long> {
 
     public Optional<MyData> findById(Long name);
+    public List<MyData> findByNameLike(String name);
+    public List<MyData> findByIdIsNotNullOrderByIdDesc();
+    public List<MyData> findByAgeGreaterThan(Integer age);
+    public List<MyData> findByAgeBetween(Integer age1, Integer age2);
 
 }
