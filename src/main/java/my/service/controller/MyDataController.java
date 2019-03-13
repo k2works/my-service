a2188f951b380b44d8e64421be1d40fca7eacdc6
k2works <kakimomokuri@gmail.com>
2019-03-13 13:28:40 +0900

    feat: エンティティのCRUD
    データの初期化処理

--- src/main/java/my/service/controller/MyDataController.java
+++ src/main/java/my/service/controller/MyDataController.java
@@ -10,11 +10,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.PostConstruct;
+
 @Controller
 public class MyDataController {
 
     @Autowired
     MyDataRepository repository;
+
+    @PostConstruct
+    public void init() {
+        // 1つめのダミーデータ作成
+        MyData d1 = new MyData();
+        d1.setName("tuyano");
+        d1.setAge(123);
+        d1.setMail("syoda@tuyano.com");
+        d1.setMemo("this is my data!");
+        repository.saveAndFlush(d1);
+        // 2つめのダミーデータ作成
+        MyData d2 = new MyData();
+        d2.setName("hanako");
+        d2.setAge(15);
+        d2.setMail("hanako@flower");
+        d2.setMemo("my girl friend.");
+        repository.saveAndFlush(d2);
+        // 3つめのダミーデータ作成
+        MyData d3 = new MyData();
+        d3.setName("sachiko");
+        d3.setAge(37);
+        d3.setMail("sachico@happy");
+        d3.setMemo("my work friend...");
+        repository.saveAndFlush(d3);
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(
@@ -23,7 +50,7 @@ public class MyDataController {
         mav.setViewName("index");
         mav.addObject("msg","this is sample content.");
         Iterable<MyData> list = repository.findAll();
-        mav.addObject("dataList", list);
+        mav.addObject("datalist", list);
         return mav;
     }
 
