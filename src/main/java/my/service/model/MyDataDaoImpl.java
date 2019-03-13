118edd83e0a584593df42efd89d61ac1ae37a925
k2works <kakimomokuri@gmail.com>
2019-03-13 16:08:07 +0900

    feat: EntityManagerによるデータベースアクセス
    DAOクラスの実装

---
+++ src/main/java/my/service/model/MyDataDaoImpl.java
@@ -0,0 +1,31 @@
+package my.service.model;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class MyDataDaoImpl implements MyDataDao<MyData> {
+   private static final long serialVersionUID = 1L;
+
+   private EntityManager entityManager;
+
+   public MyDataDaoImpl(){
+       super();
+   }
+   public MyDataDaoImpl(EntityManager manager){
+       this();
+       entityManager = manager;
+   }
+
+   @Override
+    public List<MyData> getAll() {
+       Query query = entityManager.createQuery("from MyData");
+       @SuppressWarnings("unchecked")
+       List<MyData> list = query.getResultList();
+       entityManager.close();
+       return list;
+   }
+}
