2e4d44f52d97f0695d5c49f1f1172ef5f35fc15c
k2works <kakimomokuri@gmail.com>
2019-03-13 13:09:29 +0900

    feat: エンティティのCRUD
    コントローラを修正する

--- src/main/java/my/service/controller/MyDataController.java
+++ src/main/java/my/service/controller/MyDataController.java
@@ -2,9 +2,12 @@ package my.service.controller;
 
 import my.service.model.MyData;
 import my.service.repository.MyDataRepository;
+import org.h2.engine.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,12 +16,22 @@ public class MyDataController {
     @Autowired
     MyDataRepository repository;
 
-    @RequestMapping("/")
-    public ModelAndView index(ModelAndView mav) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(
+            @ModelAttribute("formModel") MyData myData,
+            ModelAndView mav) {
         mav.setViewName("index");
         mav.addObject("msg","this is sample content.");
         Iterable<MyData> list = repository.findAll();
-        mav.addObject("data", list);
+        mav.addObject("dataList", list);
         return mav;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView form(
+            @ModelAttribute("formModel") MyData myData,
+            ModelAndView mav) {
+        repository.saveAndFlush(myData);
+        return new ModelAndView("redirect:/");
     }
 }
