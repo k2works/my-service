219a748c7bab168cb0d214f59725790e74b0c8ba
k2works <kakimomokuri@gmail.com>
2019-03-14 16:49:57 +0900

    feat: MongoDBの利用
    検索メソッドを追加する

--- src/main/java/my/service/controller/MyDataMongoController.java
+++ src/main/java/my/service/controller/MyDataMongoController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class MyDataMongoController {
 
@@ -33,5 +35,33 @@ public class MyDataMongoController {
         MyDataMongo mydata = new MyDataMongo(name, memo);
         repository.save(mydata);
         return new ModelAndView("redirect:/mongo");
+    }
+
+    @RequestMapping(value = "/mongo/find", method = RequestMethod.GET)
+    public ModelAndView find(ModelAndView mav) {
+        mav.setViewName("mongo_find");
+        mav.addObject("title","Find Page");
+        mav.addObject("msg","MyDataMongoのサンプルです。");
+        mav.addObject("value","");
+        List<MyDataMongo> list = repository.findAll();
+        mav.addObject("datalist",list);
+        return mav;
+    }
+
+    @RequestMapping(value = "/mongo/find", method = RequestMethod.POST)
+    public ModelAndView search(
+            @RequestParam("find") String param,
+            ModelAndView mav) {
+        mav.setViewName("mongo_find");
+        if (param == ""){
+            mav = new ModelAndView("redirect:/mongo/find");
+        } else {
+            mav.addObject("title", "Find result");
+            mav.addObject("msg","「" + param + "」の検索結果");
+            mav.addObject("value",param);
+            List<MyDataMongo> list = repository.findByName(param);
+            mav.addObject("datalist", list);
+        }
+        return mav;
     }
 }
--- src/main/java/my/service/repository/MyDataMongoRepository.java
+++ src/main/java/my/service/repository/MyDataMongoRepository.java
@@ -4,6 +4,9 @@ import my.service.model.MyDataMongo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MyDataMongoRepository extends MongoRepository<MyDataMongo, Long> {
+    public List<MyDataMongo> findByName(String s);
 }
