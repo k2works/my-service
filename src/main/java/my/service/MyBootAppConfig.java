71c10ff23f42d276bdf5b98ee07dc40f4556d81a
k2works <kakimomokuri@gmail.com>
2019-03-14 13:06:36 +0900

    feat: 覚えておきたいその他の機能
    Beanを登録して利用する

--- src/main/java/my/service/MyBootAppConfig.java
+++ src/main/java/my/service/MyBootAppConfig.java
@@ -1,7 +1,14 @@
 package my.service;
 
+import my.service.component.MyDataBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyBootAppConfig {
+
+    @Bean
+    MyDataBean myDataBean() {
+        return new MyDataBean();
+    }
 }
